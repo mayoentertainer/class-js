1bfb8ae7d994d3eb2af34ce0d6312924d819fbe4
mayoentertainer <smartmayowa9@gmail.com>
2026-06-16 13:30:01 +0100

    hiiiii

--- assss.js
+++ assss.js
@@ -1,20 +1,15 @@
-/*Create a function named checkEntry.
-​It should accept two parameters: age (a number) and isVIP (a boolean: true or false).
-​Rule 1: If isVIP is true, return "Access Granted: Welcome VIP!" immediately.
-​Rule 2: If they are not a VIP, they must be 21 or older. If they are under 21, return "Access Denied: Too young!".
-​Rule 3: If they pass both checks, return "Access Granted: Enjoy the night!".*/
 
-
-
-function checkEntry(age, isVIP) {
-    if (isVIP === true){
-        return "Acess Granted, Welcome VIP";
-    }
-    else if(age < 21){
-        return "Acess Denied, Too young";
-    }
-    else{
-        return "Acess Granted, Enjoy the night";
-    }
+function calcCircumfrence(radius){
+    ans = 2 * Math.PI * radius
+    console.log("The circumference is" + ans)
 }
-console.log(checkEntry(21, true))
+
+calcCircumfrence(5)
+
+
+function calcArea(radius){
+    anss = Math.PI * radius * radius
+    console.log("The Area is" + anss)
+}
+
+calcArea(5)
